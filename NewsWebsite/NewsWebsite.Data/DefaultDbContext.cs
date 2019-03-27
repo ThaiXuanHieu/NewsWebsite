77c43f5bf3e61ad29c00a8ae3abc0c5a05fd501d
Hieu <s2freedom6789@gmail.com>
2019-03-27 09:33:19 +0700

    Update Data Layer

---
+++ NewsWebsite/NewsWebsite.Data/DefaultDbContext.cs
@@ -0,0 +1,34 @@
+namespace NewsWebsite.Data
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+    using NewsWebsite.Data.Entities;
+
+    public partial class DefaultDbContext : DbContext
+    {
+        public DefaultDbContext()
+            : base("name=DefaultDbContext")
+        {
+        }
+
+        public virtual DbSet<Category> Categories { get; set; }
+        public virtual DbSet<Customer> Customers { get; set; }
+        public virtual DbSet<Function> Functions { get; set; }
+        public virtual DbSet<Post> Posts { get; set; }
+        public virtual DbSet<PostComment> PostComments { get; set; }
+        public virtual DbSet<PostRate> PostRates { get; set; }
+        public virtual DbSet<Role> Roles { get; set; }
+        public virtual DbSet<RoleFunctionRelationship> RoleFunctionRelationships { get; set; }
+        public virtual DbSet<User> Users { get; set; }
+        public virtual DbSet<UserRoleRelationship> UserRoleRelationships { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+                .Property(e => e.LastName)
+                .IsFixedLength();
+        }
+    }
+}
--- NewsWebsite/NewsWebsite/Areas/Admin/AdminAreaRegistration.cs
+++ NewsWebsite/NewsWebsite/Areas/Admin/AdminAreaRegistration.cs
@@ -17,7 +17,8 @@ namespace NewsWebsite.Areas.Admin
             context.MapRoute(
                 "Admin_default",
                 "Admin/{controller}/{action}/{id}",
-                new { action = "Index", id = UrlParameter.Optional }
+                new { action = "Index", id = UrlParameter.Optional },
+                new[] { "NewsWebsite.Areas.Admin.Controllers" }
             );
         }
     }
