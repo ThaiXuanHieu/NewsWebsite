205d293624f1d8796b2f7a893db2b646246a4826
ThaiXuanHieu <48479522+ThaiXuanHieu@users.noreply.github.com>
2019-12-05 17:06:36 +0700

    Update

--- NewsWebsite/NewsWebsite/Areas/Admin/Assets/js/plugins/ckeditor/config.js
+++ NewsWebsite/NewsWebsite/Areas/Admin/Assets/js/plugins/ckeditor/config.js
@@ -7,4 +7,15 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
+    config.syntaxhighlight_lang = 'csharp';
+    config.syntaxhighlight_hideControls = true;
+    config.language = 'vi';
+    config.filebrowserBrowseUrl = '/Admin/Assets/js/plugins/ckfinder/ckfinder.html';
+    config.filebrowserImageBrowseUrl = '/Admin/Assets/js/plugins/ckfinder.html?Type=Images';
+    config.filebrowserFlashBrowseUrl = '/Admin/Assets/js/plugins/ckfinder.html?Type=Flash';
+    config.filebrowserUploadUrl = '/Admin/Assets/js/plugins/ckfinder/core/connector/aspx/connector.aspx?command=QuickUpload&type=Files';
+    config.filebrowserImageUploadUrl = '/Admin/UploadImage';
+    config.filebrowserFlashUploadUrl = '/Admin/Assets/js/plugins/ckfinder/core/connector/aspx/connector.aspx?command=QuickUpload&type=Flash';
+
+    CKFinder.setupCKEditor(null, '/Admin/Assets/js/plugins/ckfinder/');
 };
